b15a5650adf8b49643115ed311510b4fd5a60bc2
ThanhTris <thanhtri005@gmail.com>
2024-07-18 15:44:09 +0700

    test

--- bt3.js
+++ bt3.js
@@ -2,8 +2,8 @@ function checkPalindrome(str) {
     let reversed = str.split("").reverse().join("")
     return str === reversed
 }
-let str1 = "anna";
+let str1 = "anna"
 if (checkPalindrome(str1))
-    console.log("Chuoi do la Palidrome");
+    console.log("Chuoi do la Palidrome")
 else
-    console.log("chuoi do khong phai la palidrome");
+    console.log("chuoi do khong phai la palidrome")
